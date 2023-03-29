70f0140220bdb7fb6b72c5c8db6f997ca76814a2
Aftertaste26 <20hardrock12@gmail.com>
2023-03-29 23:02:07 +0800

    fixed output receiving NaN value

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
 #include <SerialCommand.h>     //Library for serial communication with the Arduino; SerialCommand by Stefan Rado
 #include <EEPROM.h>
 
-#define ONE_WIRE_BUS 2      // Pin connected to DS18B20 data line
-#define COOLING_PIN_A 3     // Pin A of H-bridge connected to cooling element
-#define COOLING_PIN_B 4     // Pin B of H-bridge connected to cooling element
-#define COOLING_SPEED 200   // PWM value (0-255) for controlling cooling element speed
-#define TARGET_TEMP 4       // Target temperature in Celsius
-#define CHECK_INTERVAL 1000 // Time between temperature checks in milliseconds
-#define KP 5.0              // Proportional constant for PID control
-#define KI 0.1              // Integral constant for PID control
-#define KD 1.0              // Derivative constant for PID control
+#define ONE_WIRE_BUS 2     // Pin connected to DS18B20 data line
+#define COOLING_PIN_A 3    // Pin A of H-bridge connected to cooling element
+#define COOLING_PIN_B 4    // Pin B of H-bridge connected to cooling element
+#define COOLING_SPEED 200  // PWM value (0-255) for controlling cooling element speed
+#define CHECK_INTERVAL 500 // Time between temperature checks in milliseconds
+double TARGET_TEMP = 40;   // Target temperature in Celsius
+double KP = 5.0;           // Proportional constant for PID control
+double KI = 0.1;           // Integral constant for PID control
+double KD = 1.0;           // Derivative constant for PID control
 
 // Define the EEPROM addresses for the PID values
 #define PID_P_ADDRESS 0
@@ -31,11 +31,12 @@ SerialCommand sCmd;
 
 // Variables for storing time information
 unsigned long previousMillis = 0;
+unsigned long lcdMillis = 0;
 unsigned long uptimeSeconds = 0;
 
 // Variables for PID control
-double input, output, setpoint;
-PID pid(&input, &output, &setpoint, KP, KI, KD, DIRECT);
+double Input, Output, Setpoint;
+PID myPID(&Input, &Output, &Setpoint, KP, KI, KD, DIRECT);
 
 void setKP()
 {
@@ -43,7 +44,7 @@ void setKP()
   double kp = atof(sCmd.next());
 
   // Set the proportional constant for PID control
-  pid.SetTunings(kp, KI, KD);
+  myPID.SetTunings(kp, KI, KD);
 
   Serial.print(F("Proportional constant set to: "));
   Serial.println(kp);
@@ -55,7 +56,7 @@ void setKI()
   double ki = atof(sCmd.next());
 
   // Set the integral constant for PID control
-  pid.SetTunings(KP, ki, KD);
+  myPID.SetTunings(KP, ki, KD);
 
   Serial.print(F("Integral constant set to: "));
   Serial.println(ki);
@@ -67,7 +68,7 @@ void setKD()
   double kd = atof(sCmd.next());
 
   // Set the derivative constant for PID control
-  pid.SetTunings(KP, KI, kd);
+  myPID.SetTunings(KP, KI, kd);
 
   Serial.print(F("Derivative constant set to: "));
   Serial.println(kd);
@@ -76,16 +77,16 @@ void setKD()
 void getpidCommand()
 {
   char output[50];
-  snprintf(output, sizeof(output), "Current PID values: KP=%.2f, KI=%.2f, KD=%.2f", pid.GetKp(), pid.GetKi(), pid.GetKd());
+  snprintf(output, sizeof(output), "Current PID values: KP=%.2f, KI=%.2f, KD=%.2f", myPID.GetKp(), myPID.GetKi(), myPID.GetKd());
   Serial.println(output);
 }
 
 // Function to save the current PID values to EEPROM
 void savePID()
 {
-  EEPROM.put(PID_P_ADDRESS, pid.GetKp());
-  EEPROM.put(PID_I_ADDRESS, pid.GetKi());
-  EEPROM.put(PID_D_ADDRESS, pid.GetKd());
+  EEPROM.put(PID_P_ADDRESS, myPID.GetKp());
+  EEPROM.put(PID_I_ADDRESS, myPID.GetKi());
+  EEPROM.put(PID_D_ADDRESS, myPID.GetKd());
 }
 
 void initPID()
@@ -101,7 +102,7 @@ void initPID()
   // If PID values are not set in EEPROM, set default values
   if (isnan(kp) || isnan(ki) || isnan(kd))
   {
-    pid.SetTunings(kp, ki, kd);
+    myPID.SetTunings(KP, KI, KD);
     savePID(); // Save default PID values to EEPROM
   }
 }
@@ -111,23 +112,30 @@ void setup()
 
   initPID();
 
-  // Set cooling element pins as outputs
+  // Set cooling element pins as Outputs
   pinMode(COOLING_PIN_A, OUTPUT);
   pinMode(COOLING_PIN_B, OUTPUT);
+
   // Initialize DS18B20 temperature sensor
   sensors.begin();
+
   // Initialize I2C LCD
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("FrostGuard v1.0");
+
   // Set the target temperature for PID control
-  setpoint = TARGET_TEMP;
+  Setpoint = TARGET_TEMP;
+
   // Set the PID mode to automatic
-  pid.SetMode(AUTOMATIC);
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetSampleTime(1000);
+  myPID.SetOutputLimits(-255, 255);
 
   // Initialize serial communication with a baud rate of 9600
   Serial.begin(9600);
+
   // Add commands to set PID constants via serial communication
   sCmd.addCommand("setKP", setKP);
   sCmd.addCommand("setKI", setKI);
@@ -144,45 +152,37 @@ void loop()
   // Read serial commands from Arduino
   sCmd.readSerial();
 
-  // Record current time in milliseconds
-  unsigned long currentMillis = millis();
-  // Calculate elapsed time since last temperature check
-  unsigned long elapsedMillis = currentMillis - previousMillis;
-  previousMillis = currentMillis;
-  // Update uptime in seconds
-  uptimeSeconds += elapsedMillis / 1000;
+  // Record current time in seconds
+  uptimeSeconds = millis() / 1000;
 
   // Request temperature data from DS18B20
   sensors.requestTemperatures();
+  
   // Read temperature data from the first sensor (there is only one in this example)
-  input = sensors.getTempCByIndex(0);
-
-  // Calculate PID output based on current temperature and target temperature
-  pid.Compute();
+  Input = sensors.getTempCByIndex(0);
 
   // Display temperature and uptime on the first line of the LCD
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
-  lcd.print(input);
+  lcd.print(Input);
   lcd.print((char)223); // Degree symbol
   lcd.print("C   Uptime: ");
   lcd.print(uptimeSeconds);
   lcd.print("s");
 
-  // Set cooling element speed based on PID output
-  if (output >= 0)
+  myPID.Compute();
+
+  // Set cooling element speed based on PID Output
+  if (Output >= 0)
   {
     // Cooling element is turned on in forward direction
-    analogWrite(COOLING_PIN_A, COOLING_SPEED);
+    analogWrite(COOLING_PIN_A, Output);
     analogWrite(COOLING_PIN_B, 0);
   }
   else
   {
     // Cooling element is turned on in reverse direction
     analogWrite(COOLING_PIN_A, 0);
-    analogWrite(COOLING_PIN_B, COOLING_SPEED);
+    analogWrite(COOLING_PIN_B, abs(Output));
   }
-
-  // Wait for the specified interval before checking temperature again
-  delay(CHECK_INTERVAL);
 }
